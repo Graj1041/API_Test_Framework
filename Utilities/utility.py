9fbf909283e54a5eefef1f9dbd8a5f76ff5cb92c
Raj Kumar Gupta <35459315+Graj1041@users.noreply.github.com>
2020-08-23 20:24:49 +0530

    Add files via upload

--- Utilities/utility.py
+++ Utilities/utility.py
@@ -1,7 +1,9 @@
+import jsonschema
 from jsonschema import validate
 
 
 def assert_valid_schema(j_response):
+
     schema = {
         "type": "object",
         "properties": {
@@ -12,5 +14,10 @@ def assert_valid_schema(j_response):
         },
     }
 
-    return validate(instance=j_response, schema=schema)
+    try:
+        validate(instance=j_response, schema=schema)
+    except jsonschema.exceptions.ValidationError as err:
+        return False
+    return True
+
 
